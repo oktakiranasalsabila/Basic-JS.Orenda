eeb9cb0e11bc5cd6d4cee3bfa844a308b2d74ff0
oktakiranasalsabila <oktakiranasalsabila@gmail.com>
2024-02-21 11:21:44 +0700

    latihan

---
+++ TES/latihan 2/angkot2.js
@@ -0,0 +1,19 @@
+var jmlAngkot =  10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+
+while (noAngkot <= angkotBeroperasi) {
+    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik. ');
+    noAngkot++;
+}
+
+// for (noAngkot = 7; noAngkot<= jmlAngkot; noAngkot++ )
+//  {
+//     console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi. ');
+    
+// }
+for (noAngkot = angkotBeroperasi + 1; noAngkot<= jmlAngkot; noAngkot++ )
+ {
+    console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi. ');
+    
+}
