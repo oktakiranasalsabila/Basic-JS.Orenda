c9ac86261fd70689edeef550a6ef1c3a6a350fc7
oktakiranasalsabila <114393511+oktakiranasalsabila@users.noreply.github.com>
2024-02-21 11:42:04 +0700

    Delete TES directory

--- TES/latihan 1/angkot1.js
+++
@@ -1,5 +0,0 @@
-var nilaiAwal = 1;
-while (nilaiAwal <= 10) {
-    console.log('Angkot No.' + nilaiAwal + ' beroperasi dengan baik.');
-nilaiAwal ++;
-}
--- TES/latihan 3/angkot3.js
+++
@@ -1,9 +0,0 @@
-for (noAngkot= 1; noAngkot<= 10; noAngkot++) {
-   if (noAngkot <= 6) {
-     console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.' );
-   }
-   else{
-    console.log('Angkot No. ' + noAngkot + ' tidak beroperasi.' );
-   }
-    
-}
